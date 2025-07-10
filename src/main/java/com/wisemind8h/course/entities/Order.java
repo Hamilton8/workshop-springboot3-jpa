2c055456b34c44ee725daf85484c744f9c794d5d
Hamilton Guirrugo <guirrugohamiltonantonio@gmail.com>
2025-07-09 19:28:40 -0700

    OrderStatus enum

--- src/main/java/com/wisemind8h/course/entities/Order.java
+++ src/main/java/com/wisemind8h/course/entities/Order.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonFormatTypes;
+import com.wisemind8h.course.entities.enums.OrderStatus;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -32,13 +33,16 @@ public class Order implements Serializable{
 	@ManyToOne
 	private User client;
 	
+	private Integer orderStatus;
+	
 	public Order() {
 		
 	}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 
@@ -64,6 +68,16 @@ public class Order implements Serializable{
 
 	public void setClient(User client) {
 		this.client = client;
+	}
+ 
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if(orderStatus!=null) {
+			this.orderStatus = orderStatus.getCode();
+		}
 	}
 
 	@Override
