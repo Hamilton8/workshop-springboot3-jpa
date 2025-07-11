811050dc6259a4c01b5345eff393f3041025c2ee
Hamilton Guirrugo <guirrugohamiltonantonio@gmail.com>
2025-07-10 19:33:10 -0700

    Many-To-Many association with JoinTable

--- src/main/java/com/wisemind8h/course/config/TestConfig.java
+++ src/main/java/com/wisemind8h/course/config/TestConfig.java
@@ -41,13 +41,6 @@ public class TestConfig implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		 
-		User u1 = new User(null, "Maria BROWN", "maria@gmail.com", "1234321", "12345");
-		User u2 = new User(null, "Alex Green", "alex@gmail.com", "12348765", "12345");
-		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"),OrderStatus.PAID, u1);
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT,u2);
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT,u1);
-		
 		Category cat1 = new Category(null, "Electronics");
 		Category cat2 = new Category(null, "Books");
 		Category cat3 = new Category(null, "Computers");
@@ -58,10 +51,29 @@ public class TestConfig implements CommandLineRunner{
 		Product prod4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0," ");
 		Product prod5 = new Product(null, "Rails for Dumies", "Cras fringilla convallis sem vel faucibus", 100.99, " " );
 		
+		categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
+		productRepository.saveAll(Arrays.asList(prod1,prod2,prod3,prod4,prod5));
+		
+		
+		prod1.getCategories().add(cat2);
+		prod2.getCategories().add(cat1);
+		prod2.getCategories().add(cat3);
+		prod3.getCategories().add(cat3);
+		prod4.getCategories().add(cat3);
+		prod5.getCategories().add(cat2); 
+		
+		productRepository.saveAll(Arrays.asList(prod1,prod2,prod3,prod4,prod5));
+		
+		
+		User u1 = new User(null, "Maria BROWN", "maria@gmail.com", "1234321", "12345");
+		User u2 = new User(null, "Alex Green", "alex@gmail.com", "12348765", "12345");
+		
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"),OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT,u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT,u1);
 		
 		userRepository.saveAll(Arrays.asList(u1,u2));
 		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
-		categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
-		productRepository.saveAll(Arrays.asList(prod1,prod2,prod3,prod4,prod5));
+		
 	}
 }
